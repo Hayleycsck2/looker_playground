fe17c49a3dddad47b2b10796e673aa702080dfef
Hayley Knight <48437782+Hayleycsck2@users.noreply.github.com>
2021-02-22 16:51:14 +0000

    Add files via upload

---
+++ Basic Box Chart Visualisation.js
@@ -0,0 +1,128 @@
+//Basic Box Chart Visualisation
+
+/**
+ * Welcome to the Looker Visualization Builder! Please refer to the following resources 
+ * to help you write your visualization:
+ *  - API Documentation - https://github.com/looker/custom_visualizations_v2/blob/master/docs/api_reference.md
+ *  - Example Visualizations - https://github.com/looker/custom_visualizations_v2/tree/master/src/examples
+ **/
+
+const visObject = {
+ /**
+  * Configuration options for your visualization. In Looker, these show up in the vis editor
+  * panel but here, you can just manually set your default values in the code.
+  **/
+  options: {
+    first_option: {
+      type: "string",
+      label: "My First Option",
+      default: "Default Value"
+    },
+    second_option: {
+      type: "number",
+      label: "My Second Option",
+      default: 42
+    }
+  },
+ 
+ /**
+  * The create function gets called when the visualization is mounted but before any
+  * data is passed to it.
+  **/
+  create: function(element, config){
+    element.innerHTML = "<h1>Ready to render!</h1>";
+  },
+
+ /**
+  * UpdateAsync is the function that gets called (potentially) multiple times. It receives
+  * the data and should update the visualization with the new data.
+  **/
+  updateAsync: function(data, element, config, queryResponse, details, doneRendering){
+
+
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 40},
+  width = 400 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#vis")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+    const Measure1 = queryResponse.fields.measures[0].name //returns game.count 
+
+    
+    formattedData = []
+
+    // format the data
+    data.forEach(function(d) {
+      formattedData.push(
+        d[Measure1]["value"]
+      );
+    });
+
+var data = formattedData
+var maxHieght = Math.max(...data) +100
+
+// Compute summary statistics used for the box:
+var data_sorted = data.sort(d3.ascending)
+var q1 = d3.quantile(data_sorted, .25)
+var median = d3.quantile(data_sorted, .5)
+var q3 = d3.quantile(data_sorted, .75)
+var interQuantileRange = q3 - q1
+var min = q1 - 1.5 * interQuantileRange
+var max = q1 + 1.5 * interQuantileRange
+
+// Show the Y scale
+var y = d3.scaleLinear()
+  .domain([0,maxHieght]) // need setting to maxHieght
+  .range([height, 0]);
+svg.call(d3.axisLeft(y))
+
+// a few features for the box
+var center = 200
+var width = 100
+
+// Show the main vertical line
+svg
+.append("line")
+  .attr("x1", center)
+  .attr("x2", center)
+  .attr("y1", y(min) )
+  .attr("y2", y(max) )
+  .attr("stroke", "black")
+
+// Show the box
+svg
+.append("rect")
+  .attr("x", center - width/2)
+  .attr("y", y(q3) )
+  .attr("height", (y(q1)-y(q3)) )
+  .attr("width", width )
+  .attr("stroke", "black")
+  .style("fill", "#69b3a2")
+
+// show median, min and max horizontal lines
+svg
+.selectAll("toto")
+.data([min, median, max])
+.enter()
+.append("line")
+  .attr("x1", center-width/2)
+  .attr("x2", center+width/2)
+  .attr("y1", function(d){ return(y(d))} )
+  .attr("y2", function(d){ return(y(d))} )
+  .attr("stroke", "black")
+ 
+    
+
+    doneRendering()
+  }
+};
+
+looker.plugins.visualizations.add(visObject);
