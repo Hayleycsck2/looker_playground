20e2e97ea4e0f4d8f5bb2621b90773f6501edf70
Hayley Knight <48437782+Hayleycsck2@users.noreply.github.com>
2021-03-05 09:45:36 +0000

    Update Basic Box Chart Visualisation.js

--- Basic Box Chart Visualisation.js
+++ Basic Box Chart Visualisation.js
@@ -45,6 +45,8 @@ var margin = {top: 10, right: 30, bottom: 30, left: 40},
   width = 400 - margin.left - margin.right,
   height = 400 - margin.top - margin.bottom;
 
+element.innerHTML = ""
+   
 // append the svg object to the body of the page
 var svg = d3.select("#vis")
 .append("svg")
